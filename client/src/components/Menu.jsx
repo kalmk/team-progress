be67bfe605be2be7bd8fac0f7bc25a7fb7bedf84
kalmk <111930167+kalmk@users.noreply.github.com>
2024-10-25 15:53:49 -0700

    Issue #3 done

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import LamaTube from "../img/logo.png";
+import HelloTube from "../img/logo.png";
 import HomeIcon from "@mui/icons-material/Home";
 import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
 import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
@@ -90,8 +90,8 @@ const Menu = ({ darkMode, setDarkMode }) => {
       <Wrapper>
         <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
           <Logo>
-            <Img src={LamaTube} />
-            LamaTube
+            <Img src={HelloTube} />
+            HelloTube
           </Logo>
         </Link>
         <Item>
@@ -137,7 +137,7 @@ const Menu = ({ darkMode, setDarkMode }) => {
             <Hr />
           </>
         }
-        <Title>BEST OF LAMATUBE</Title>
+        <Title>BEST OF HelloTube</Title>
         <Item>
           <LibraryMusicOutlinedIcon />
           Music
